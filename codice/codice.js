89b544a647f4e56a0370337795a90b85a31a934d
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-02-01 00:07:43 +0100

    fine esercizio usando la modifica del dom ovvero l'inserimento di elementi con js ( da ignorare le animazioni)

--- codice/codice.js
+++ codice/codice.js
@@ -9,32 +9,69 @@
 const grid = document.querySelector("#grid");
 
 // funzione for con il quale ottimizzeremo la creazione dei quadrati
-for(let i = 0 ; i <= 100 ;i++ )
+for(let i = 0 ; i <= 100 ; i++ )
 {
+    // comando usato per creare nuovacostante a cui abbiamo assegnato 
+    const newelement = document.createElement("div")
+
+
+    newelement.className=("square");
+    
+
     // prima condizione usata per " inserire" fuzzbuzz solo quando ci sono numeri multipli per 3 e 5
     if(i % 5 == 0 && i % 3 == 0)
     {   
         // grid è un oggetto DOM (Document Object Model) che rappresenta un elemento HTML, e innerHTML è una proprietà che restituisce o imposta il markup HTML contenuto all'interno di quell'elemento. sta aggiungendo una nuova <div> con la classe "square" e il testo "buzz" all'interno.
-        grid.innerHTML += `<div class="square">${"fuzzbuzz"}</div>`
+        
+        // inizio righe di stilizzazione assegnate da js
+        newelement.innerHTML+= `${"fizzbuzz"}`;
+        newelement.style.color = "orange";
+        newelement.style.backgroundColor = "lightgreen";
+        newelement.style.fontSize= "bold";
+        // fine righe di stilizzazione assegnate da js
+
+        newelement.className=("rotate");
+        
 
     }
     else if( i % 3 == 0)
     {
          // grid è un oggetto DOM (Document Object Model) che rappresenta un elemento HTML, e innerHTML è una proprietà che restituisce o imposta il markup HTML contenuto all'interno di quell'elemento. sta aggiungendo una nuova <div> con la classe "square" e il testo "fuzz" all'interno.
-        grid.innerHTML += `<div class="square">${"fuzz"}</div>`
+        
+        // inizio righe di stilizzazione assegnate da js
+         newelement.innerHTML+= `${"fizz"}`;
+         newelement.style.color = "blue";
+         newelement.style.backgroundColor = "red";
+         newelement.style.fontSize= "bold";
+        // fine righe di stilizzazione assegnate da js
 
+        // newelement.className=("pulse");
     }
     else if(i % 5 == 0)
     {   
         // grid è un oggetto DOM (Document Object Model) che rappresenta un elemento HTML, e innerHTML è una proprietà che restituisce o imposta il markup HTML contenuto all'interno di quell'elemento. sta aggiungendo una nuova <div> con la classe "square" e il testo "buzz" all'interno.
-        grid.innerHTML += `<div class="square">${"buzz"}</div>`
+        // La proprietà innerHTML in JavaScript viene utilizzata per ottenere o impostare il contenuto HTML di un elemento.
+        newelement.innerHTML+= `${"buzz"}`;
+
+        // inizio righe di stilizzazione assegnate da js
+        newelement.style.color = "purple";
+        newelement.style.backgroundColor = "yellow";
+        newelement.style.fontSize= "bold";
+        // fine righe di stilizzazione assegnate da js
+
+        newelement.className=("slide");
 
     }
     else
     {
-
-        grid.innerHTML += `<div class="square">${i}</div>`
+        // stringa usata per inserire all' interno delle rimanenti caselle il valore di i
+        newelement.innerHTML+=`${i}`;
     }
-    
+
+    //  alla fine "appendiamo" l'elemento appena creato (newElement)
+    // alla griglia (grigliaElement)
+    // il metodo .append() aggiunge all'interno di grigliaElement
+    // l'elemento che gli passiamo tra parentesi, esso sarà l'ultimo figlio
+    grid.append(newelement);
     
 }
